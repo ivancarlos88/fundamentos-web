ec53558562c6040fa9f11fdbef3c5ea82ce50397
ivancarlos88 <ivancarlos.ms.21@gmail.com>
2021-10-26 13:55:27 -0300

    atualizando js

---
+++ Atividades/PáginaWeb2/assets/js/script.js
@@ -0,0 +1,10 @@
+let cao = document.querySelector('#cao')
+
+function imagemAmp(){
+    cao.style.width = '800px'
+    cao.style.height = '800px'
+}
+function imagemRed(){
+    cao.style.width = '400px'
+    cao.style.width = '400px'
+}
